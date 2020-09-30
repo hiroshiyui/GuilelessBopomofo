9d6e179d711356fe19f7eb65ddae0a772d47924a
Hui-Hong You <hiroshi@ghostsinthelab.org>
2020-09-30 16:22:42 +0800

    WIP: Add _chewingInit JNI function
    Signed-off-by: Hui-Hong You <hiroshi@ghostsinthelab.org>

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,5 +1,6 @@
 #include <jni.h>
 #include <string>
+#include <chewing.h>
 
 extern "C" JNIEXPORT jstring JNICALL
 Java_org_ghostsinthelab_apps_guilelessbopomofo_MainActivity_stringFromJNI(
@@ -7,4 +8,14 @@ Java_org_ghostsinthelab_apps_guilelessbopomofo_MainActivity_stringFromJNI(
         jobject /* this */) {
     std::string hello = "Hello from C++";
     return env->NewStringUTF(hello.c_str());
+}
+
+//Chewing JNI functions
+extern "C" JNIEXPORT jlong JNICALL
+Java_org_ghostsinthelab_apps_guilelessbopomofo_MainActivity_chewingInit(
+        JNIEnv* env,
+        jobject) {
+    ChewingContext *ctx;
+    ctx = chewing_new2("assets/chewing", "." "/test.sqlite3", NULL, 0);
+    return (long)ctx;
 }
