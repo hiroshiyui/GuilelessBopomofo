a0f59432e09f7430bd981543367c4804a4c9c8ec
Hui-Hong You <hiroshi@ghostsinthelab.org>
2020-10-02 17:15:00 +0800

    initChewingData()
    Signed-off-by: Hui-Hong You <hiroshi@ghostsinthelab.org>

--- app/src/main/java/org/ghostsinthelab/apps/guilelessbopomofo/MainActivity.kt
+++ app/src/main/java/org/ghostsinthelab/apps/guilelessbopomofo/MainActivity.kt
@@ -1,8 +1,15 @@
 package org.ghostsinthelab.apps.guilelessbopomofo
 
-import androidx.appcompat.app.AppCompatActivity
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import java.io.File
+import java.io.FileOutputStream
+import java.io.InputStream
+import java.util.zip.ZipFile
+import java.util.zip.ZipInputStream
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,6 +19,7 @@ class MainActivity : AppCompatActivity() {
 
         // Example of a call to a native method
         sample_text.text = stringFromJNI()
+        initChewingData()
     }
 
     /**
@@ -24,6 +32,69 @@ class MainActivity : AppCompatActivity() {
         // Used to load the 'native-lib' library on application startup.
         init {
             System.loadLibrary("native-lib")
+        }
+    }
+
+    private fun initChewingData() {
+        // Get app data directory
+        val applicationInfo = this.getApplicationInfo()
+        val chewingDataDir = File(applicationInfo.dataDir)
+
+        // Get app assets (bundled in APK file)
+        val assetManager = this.assets
+        val chewingAssetsPath = String.format("%s/%s", "chewing", getAbi())
+        val assetFiles = assetManager.list(chewingAssetsPath)
+
+        // Extract data.zip to data directory
+        val dataZipInputStream = ZipInputStream(assetManager.open(String.format("%s/%s", chewingAssetsPath, "data.zip")))
+        var nextEntry = dataZipInputStream.nextEntry
+
+        while (nextEntry != null) {
+            try {
+                Log.d("extract-zip", nextEntry.name)
+                val target = File(String.format("%s/%s", chewingDataDir.absolutePath, nextEntry.name))
+                val outputStream = FileOutputStream(target)
+                dataZipInputStream.copyTo(outputStream)
+                outputStream.close()
+                nextEntry = dataZipInputStream.nextEntry
+            } catch (e: java.lang.Exception) {
+                Log.e("extract-zip", e.message)
+            }
+        }
+        dataZipInputStream.close()
+
+        // Copy assets to data directory
+//        assetFiles?.let {
+//            for (asset in it) {
+//                try {
+//                    val inputStream = assetManager.open(String.format("%s/%s", chewingAssetsPath, asset))
+//                    val target = File(String.format("%s/%s", chewingDataDir.absolutePath, asset))
+//                    val outputStream = FileOutputStream(target)
+//                    Log.d("initChewingData-debug", String.format("Copying '%s' to '%s'...", asset, target.absolutePath))
+//
+//                    inputStream.copyTo(outputStream)
+//
+//                    inputStream.close()
+//                    outputStream.close()
+//
+//                    // make 'init_database' executable
+//                    if (asset == "init_database") {
+//                        target.setExecutable(true)
+//                    }
+//                } catch (e: Exception) {
+//                    Log.e("initChewingData-error", e.message);
+//                }
+//            }
+//        }
+    }
+
+    private fun getAbi(): String? {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            // on newer Android versions, we'll return only the most important Abi version
+            Build.SUPPORTED_ABIS[0]
+        } else {
+            // on pre-Lollip versions, we got only one Abi
+            Build.CPU_ABI
         }
     }
 }
