15a15b5744444932932c1a3fdec9794527ab1c6e
Hui-Hong You <hiroshi@ghostsinthelab.org>
2020-10-06 18:07:38 +0800

    Be able to commit demo string/phrase '綠茶'
    Signed-off-by: Hui-Hong You <hiroshi@ghostsinthelab.org>

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -7,7 +7,7 @@
 
 extern "C" JNIEXPORT jstring JNICALL
 Java_org_ghostsinthelab_apps_guilelessbopomofo_MainActivity_stringFromJNI(
-        JNIEnv* env,
+        JNIEnv *env,
         jobject /* this */) {
     std::string hello = "Hello from C++";
     return env->NewStringUTF(hello.c_str());
@@ -20,13 +20,13 @@ Java_org_ghostsinthelab_apps_guilelessbopomofo_MainActivity_stringFromJNI(
 /* chewing_new2() */
 extern "C" JNIEXPORT jlong JNICALL
 Java_org_ghostsinthelab_apps_guilelessbopomofo_MainActivity_chewingNew(
-        JNIEnv* env,
+        JNIEnv *env,
         jobject,
         jstring data_path) {
     ChewingContext *ctx;
 
     /* build native_data_path */
-    const char* native_data_path;
+    const char *native_data_path;
     native_data_path = env->GetStringUTFChars(data_path, JNI_FALSE);
     __android_log_print(ANDROID_LOG_VERBOSE, LOGTAG, "native_data_path: %s", native_data_path);
 
@@ -41,20 +41,141 @@ Java_org_ghostsinthelab_apps_guilelessbopomofo_MainActivity_chewingNew(
     strcat(native_user_data_path, "/");
     strcat(native_user_data_path, user_db);
 
-    __android_log_print(ANDROID_LOG_VERBOSE, LOGTAG, "native_user_data_path: %s", native_user_data_path);
+    __android_log_print(ANDROID_LOG_VERBOSE, LOGTAG, "native_user_data_path: %s",
+                        native_user_data_path);
 
     /* create chewing context */
     ctx = chewing_new2(native_data_path, native_user_data_path, nullptr, 0);
-    return (jlong)ctx;
+    return (jlong) ctx;
 }
 
 /* chewing_delete() */
 extern "C" JNIEXPORT void JNICALL
 Java_org_ghostsinthelab_apps_guilelessbopomofo_MainActivity_chewingDelete(
-        JNIEnv* env,
+        JNIEnv *env,
         jobject,
-        jlong chewing_ctx_ptr){
-    auto* ctx= reinterpret_cast<ChewingContext *>(chewing_ctx_ptr);
-    __android_log_print(ANDROID_LOG_VERBOSE, LOGTAG, "delete chewing context: %lld", (jlong)ctx);
+        jlong chewing_ctx_ptr) {
+    auto *ctx = reinterpret_cast<ChewingContext *>(chewing_ctx_ptr);
+    __android_log_print(ANDROID_LOG_VERBOSE, LOGTAG, "delete chewing context: %lld", (jlong) ctx);
     chewing_delete(ctx);
+}
+
+/* chewing_get_ChiEngMode() */
+extern "C" JNIEXPORT jint JNICALL
+Java_org_ghostsinthelab_apps_guilelessbopomofo_MainActivity_chewingGetChiEngMode(
+        JNIEnv *env,
+        jobject,
+        jlong chewing_ctx_ptr) {
+    auto *ctx = reinterpret_cast<ChewingContext *>(chewing_ctx_ptr);
+    __android_log_print(ANDROID_LOG_VERBOSE, LOGTAG,
+                        "Get chewing Chinese/English mode from context ptr: %lld", (jlong) ctx);
+    return chewing_get_ChiEngMode(ctx);
+}
+
+/* chewing_set_selKey() */
+extern "C" JNIEXPORT void JNICALL
+Java_org_ghostsinthelab_apps_guilelessbopomofo_MainActivity_chewingSetSelKey(
+        JNIEnv *env,
+        jobject,
+        jlong chewing_ctx_ptr,
+        jobject selkeys,
+        jint len) {
+    auto *ctx = reinterpret_cast<ChewingContext *>(chewing_ctx_ptr);
+    const int *keys = reinterpret_cast<const int *>(selkeys);
+    __android_log_print(ANDROID_LOG_VERBOSE, LOGTAG,
+                        "Set chewing selection keys from context ptr: %lld", (jlong) ctx);
+    chewing_set_selKey(ctx, keys, len);
+}
+
+/* chewing_get_selKey() */
+extern "C" JNIEXPORT jlong JNICALL
+Java_org_ghostsinthelab_apps_guilelessbopomofo_MainActivity_chewingGetSelKey(
+        JNIEnv *env,
+        jobject,
+        jlong chewing_ctx_ptr) {
+    auto *ctx = reinterpret_cast<ChewingContext *>(chewing_ctx_ptr);
+    __android_log_print(ANDROID_LOG_VERBOSE, LOGTAG,
+                        "Get chewing selection keys from context ptr: %lld", (jlong) ctx);
+    return (jlong) chewing_get_selKey(ctx);
+}
+
+/* chewing_free() */
+extern "C" JNIEXPORT void JNICALL
+Java_org_ghostsinthelab_apps_guilelessbopomofo_MainActivity_chewingFree(
+        JNIEnv *env,
+        jobject,
+        jlong res_ptr) {
+    auto *res = reinterpret_cast<void *>(res_ptr);
+    __android_log_print(ANDROID_LOG_VERBOSE, LOGTAG, "Free chewing resource (ptr): %lld",
+                        (jlong) res);
+    chewing_free(res);
+}
+
+/* chewing_set_maxChiSymbolLen() */
+extern "C" JNIEXPORT void JNICALL
+Java_org_ghostsinthelab_apps_guilelessbopomofo_MainActivity_chewingSetMaxChiSymbolLen(
+        JNIEnv *env,
+        jobject,
+        jlong chewing_ctx_ptr,
+        jint len) {
+    auto *ctx = reinterpret_cast<ChewingContext *>(chewing_ctx_ptr);
+    __android_log_print(ANDROID_LOG_VERBOSE, LOGTAG,
+                        "Set chewing max Chinese symbol length to %d from context ptr: %lld",
+                        (jint) len, (jlong) ctx);
+    chewing_set_maxChiSymbolLen(ctx, len);
+}
+
+/* chewing_set_candPerPage() */
+extern "C" JNIEXPORT void JNICALL
+Java_org_ghostsinthelab_apps_guilelessbopomofo_MainActivity_chewingSetCandPerPage(
+        JNIEnv *env,
+        jobject,
+        jlong chewing_ctx_ptr,
+        jint candidates) {
+    auto *ctx = reinterpret_cast<ChewingContext *>(chewing_ctx_ptr);
+    __android_log_print(ANDROID_LOG_VERBOSE, LOGTAG,
+                        "Set chewing candidates per page to %d from context ptr: %lld",
+                        (jint) candidates, (jlong) ctx);
+    chewing_set_candPerPage(ctx, candidates);
+}
+
+/* chewing_handle_Default() */
+extern "C" JNIEXPORT void JNICALL
+Java_org_ghostsinthelab_apps_guilelessbopomofo_MainActivity_chewingHandleDefault(
+        JNIEnv *env,
+        jobject,
+        jlong chewing_ctx_ptr,
+        jint key) {
+    auto *ctx = reinterpret_cast<ChewingContext *>(chewing_ctx_ptr);
+    char k = key;
+    __android_log_print(ANDROID_LOG_VERBOSE, LOGTAG,
+                        "Handle default input '%c' from context ptr: %lld", k, (jlong) ctx);
+    chewing_handle_Default(ctx, key);
+}
+
+/* chewing_handle_Enter() */
+extern "C" JNIEXPORT void JNICALL
+Java_org_ghostsinthelab_apps_guilelessbopomofo_MainActivity_chewingHandleEnter(
+        JNIEnv *env,
+        jobject,
+        jlong chewing_ctx_ptr) {
+    auto *ctx = reinterpret_cast<ChewingContext *>(chewing_ctx_ptr);
+    __android_log_print(ANDROID_LOG_VERBOSE, LOGTAG, "Handle enter input from context ptr: %lld",
+                        (jlong) ctx);
+    chewing_handle_Enter(ctx);
+}
+
+/* chewing_commit_String() */
+extern "C" JNIEXPORT jstring JNICALL
+Java_org_ghostsinthelab_apps_guilelessbopomofo_MainActivity_chewingCommitString(
+        JNIEnv *env,
+        jobject,
+        jlong chewing_ctx_ptr) {
+    auto *ctx = reinterpret_cast<ChewingContext *>(chewing_ctx_ptr);
+    __android_log_print(ANDROID_LOG_VERBOSE, LOGTAG, "Commit string from context ptr: %lld",
+                        (jlong) ctx);
+    char *commit_string = chewing_commit_String(ctx);
+    jstring ret_jstring = env->NewStringUTF(commit_string);
+    chewing_free(commit_string);
+    return ret_jstring;
 }
--- app/src/main/java/org/ghostsinthelab/apps/guilelessbopomofo/MainActivity.kt
+++ app/src/main/java/org/ghostsinthelab/apps/guilelessbopomofo/MainActivity.kt
@@ -18,11 +18,42 @@ class MainActivity : AppCompatActivity() {
 
         // Example of a call to a native method
         sample_text.text = stringFromJNI()
+
         initChewingData()
         val chewing_ctx = chewingNew(this.applicationInfo.dataDir)
-        chewing_ctx?.let {
+        chewing_ctx.let {
             Log.d(LOGTAG, it.toString())
         }
+
+        val chewing_chi_mode = chewingGetChiEngMode(chewing_ctx)
+        chewing_chi_mode.let {
+            Log.d(LOGTAG, "Chinese/English mode: ${it.toString()}")
+        }
+
+        val selKeys = arrayOf('1', '2', '3', '4', '5', '6', '7', '8', '9').map { it.toInt() }
+        chewingSetSelKey(chewing_ctx, selKeys, 9)
+
+        val listKeys = chewingGetSelKey(chewing_ctx)
+        listKeys.let {
+            Log.d(LOGTAG, "Get select key (ptr): ${it.toString()}")
+        }
+        chewingFree(listKeys)
+
+        chewingSetMaxChiSymbolLen(chewing_ctx, 10)
+        chewingSetCandPerPage(chewing_ctx, 9)
+
+        // 綠茶
+        val keys = arrayOf('x', 'm', '4', 't', '8', '6').map { it.toInt() }
+        for (key in keys) {
+            chewingHandleDefault(chewing_ctx, key)
+        }
+        chewingHandleEnter(chewing_ctx)
+
+        val commitString = chewingCommitString(chewing_ctx);
+        Log.d(LOGTAG, "Commit string: ${commitString}")
+
+        sample_text.text = commitString
+
         chewingDelete(chewing_ctx)
     }
 
@@ -34,6 +65,15 @@ class MainActivity : AppCompatActivity() {
 
     external fun chewingNew(dataPath: String): Long
     external fun chewingDelete(chewingCtx: Long)
+    external fun chewingFree(resourcePtr: Long)
+    external fun chewingGetChiEngMode(chewingCtx: Long): Int
+    external fun chewingSetSelKey(chewingCtx: Long, selKeys: List<Int>, length: Int)
+    external fun chewingGetSelKey(chewingCtx: Long): Long
+    external fun chewingSetMaxChiSymbolLen(chewingCtx: Long, length: Int)
+    external fun chewingSetCandPerPage(chewingCtx: Long, candidates: Int)
+    external fun chewingHandleDefault(chewingCtx: Long, key: Int)
+    external fun chewingHandleEnter(chewingCtx: Long)
+    external fun chewingCommitString(chewingCtx: Long): String
 
     companion object {
         // Used to load the 'native-lib' library on application startup.
