6639b8a25f89a43663715fb4ac6b1bc40ef99b56
Hui-Hong You <hiroshi@ghostsinthelab.org>
2020-10-29 21:13:07 +0800

    Declare input method service
    Signed-off-by: Hui-Hong You <hiroshi@ghostsinthelab.org>

---
+++ app/src/main/java/org/ghostsinthelab/apps/guilelessbopomofo/ChewingService.kt
@@ -0,0 +1,12 @@
+package org.ghostsinthelab.apps.guilelessbopomofo
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+class ChewingService : Service() {
+
+    override fun onBind(intent: Intent): IBinder {
+        TODO("Return the communication channel to the service.")
+    }
+}
