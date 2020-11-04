df7eac5e464d04c4f90aa8baafe9ac94c31396aa
Hui-Hong You <hiroshi@ghostsinthelab.org>
2020-11-04 14:37:14 +0800

    Declare IME basic information
    Signed-off-by: Hui-Hong You <hiroshi@ghostsinthelab.org>

--- app/src/main/java/org/ghostsinthelab/apps/guilelessbopomofo/ChewingService.kt
+++ app/src/main/java/org/ghostsinthelab/apps/guilelessbopomofo/ChewingService.kt
@@ -1,12 +1,31 @@
 package org.ghostsinthelab.apps.guilelessbopomofo
 
-import android.app.Service
-import android.content.Intent
-import android.os.IBinder
+import android.inputmethodservice.InputMethodService
+import android.view.View
+import android.view.inputmethod.EditorInfo
 
-class ChewingService : Service() {
+class ChewingService : InputMethodService() {
+    override fun onCreate() {
+        super.onCreate()
+    }
 
-    override fun onBind(intent: Intent): IBinder {
-        TODO("Return the communication channel to the service.")
+    override fun onCreateCandidatesView(): View {
+        return super.onCreateCandidatesView()
+    }
+
+    override fun onCreateInputView(): View {
+        return super.onCreateInputView()
+    }
+
+    override fun onStartInputView(info: EditorInfo?, restarting: Boolean) {
+        super.onStartInputView(info, restarting)
+    }
+
+    override fun onFinishInput() {
+        super.onFinishInput()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
     }
 }
