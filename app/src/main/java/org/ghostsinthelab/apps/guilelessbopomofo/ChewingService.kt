9231a093c7c59eec5816fd3a038f4515cc40bd5b
Hui-Hong You <hiroshi@ghostsinthelab.org>
2020-11-06 00:07:38 +0800

    WIP: Building software keyboard
    Signed-off-by: Hui-Hong You <hiroshi@ghostsinthelab.org>

--- app/src/main/java/org/ghostsinthelab/apps/guilelessbopomofo/ChewingService.kt
+++ app/src/main/java/org/ghostsinthelab/apps/guilelessbopomofo/ChewingService.kt
@@ -4,7 +4,6 @@ import android.inputmethodservice.InputMethodService
 import android.util.Log
 import android.view.View
 import android.view.inputmethod.EditorInfo
-import android.widget.Button
 
 class ChewingService : InputMethodService(), View.OnClickListener {
     val LOGTAG = "Service"
