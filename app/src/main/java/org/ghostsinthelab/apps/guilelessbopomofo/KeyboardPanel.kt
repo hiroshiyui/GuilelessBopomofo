bb4392716061430045045ac58ace0eaf95c80206
Hui-Hong You <hiroshi@ghostsinthelab.org>
2021-01-18 19:51:34 +0800

    Make width of software candidate buttons as same as possible, to improve visual comfortment
    Signed-off-by: Hui-Hong You <hiroshi@ghostsinthelab.org>

--- app/src/main/java/org/ghostsinthelab/apps/guilelessbopomofo/KeyboardPanel.kt
+++ app/src/main/java/org/ghostsinthelab/apps/guilelessbopomofo/KeyboardPanel.kt
@@ -23,7 +23,8 @@ import android.content.Context
 import android.util.AttributeSet
 import android.util.Log
 import android.widget.RelativeLayout
-import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.flexbox.FlexboxLayoutManager
 import org.ghostsinthelab.apps.guilelessbopomofo.databinding.*
 import org.ghostsinthelab.apps.guilelessbopomofo.events.*
@@ -275,7 +276,7 @@ class KeyboardPanel(
         if (!GuilelessBopomofoServiceContext.serviceInstance.physicalKeyboardPresent) {
             candidatesRecyclerView.adapter = CandidatesAdapter()
             candidatesRecyclerView.layoutManager =
-                StaggeredGridLayoutManager(4, StaggeredGridLayoutManager.HORIZONTAL)
+                GridLayoutManager(context, 4, LinearLayoutManager.HORIZONTAL, false)
         } else {
             val layoutManager = FlexboxLayoutManager(context)
             candidatesRecyclerView.adapter = PagedCandidatesAdapter(ChewingBridge.candCurrentPage())
