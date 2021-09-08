0f896defd6b32ce9226db7d08d5e0a02a88b56e2
Hui-Hong You <hiroshi@ghostsinthelab.org>
2021-09-08 11:38:57 +0800

    Update copyright & licensing information at beginning of each source code file
    Signed-off-by: Hui-Hong You <hiroshi@ghostsinthelab.org>

--- app/src/main/java/org/ghostsinthelab/apps/guilelessbopomofo/CandidateViewHolder.kt
+++ app/src/main/java/org/ghostsinthelab/apps/guilelessbopomofo/CandidateViewHolder.kt
@@ -1,6 +1,6 @@
 /*
  * Guileless Bopomofo
- * Copyright (C) 2020 YOU, HUI-HONG
+ * Copyright (C) 2021 YOU, HUI-HONG
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
