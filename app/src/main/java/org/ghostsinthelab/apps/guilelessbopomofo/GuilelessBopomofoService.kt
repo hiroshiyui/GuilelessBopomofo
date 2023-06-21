f40170d295ebcffb100a6910a4b63b73632ee8a0
Hui-Hong You <hiroshi@ghostsinthelab.org>
2023-06-21 20:23:44 +0800

    Remove unnecessary variable inputView
    Signed-off-by: Hui-Hong You <hiroshi@ghostsinthelab.org>

--- app/src/main/java/org/ghostsinthelab/apps/guilelessbopomofo/GuilelessBopomofoService.kt
+++ app/src/main/java/org/ghostsinthelab/apps/guilelessbopomofo/GuilelessBopomofoService.kt
@@ -54,7 +54,6 @@ class GuilelessBopomofoService : InputMethodService(),
     val viewBinding get() = _viewBinding!!
 
     private lateinit var sharedPreferences: SharedPreferences
-    private lateinit var inputView: Keyboard
     private val chewingDataFiles =
         listOf("dictionary.dat", "index_tree.dat", "pinyin.tab", "swkb.dat", "symbols.dat")
 
@@ -157,8 +156,7 @@ class GuilelessBopomofoService : InputMethodService(),
         Log.d(logTag, "onCreateInputView()")
         _viewBinding = KeyboardLayoutBinding.inflate(layoutInflater)
         viewBinding.keyboardPanel.switchToMainLayout()
-        inputView = viewBinding.root
-        return inputView
+        return viewBinding.root
     }
 
     override fun onInitializeInterface() {
