327b5530f981bde26c5423d73df8753ba86b3c14
Hui-Hong You <hiroshi@ghostsinthelab.org>
2023-06-23 17:27:08 +0800

    Refactor offset calibration business logic of PreEditBufferTextView#cursorMovedBy()
    Signed-off-by: Hui-Hong You <hiroshi@ghostsinthelab.org>

--- app/src/main/java/org/ghostsinthelab/apps/guilelessbopomofo/buffers/PreEditBufferTextView.kt
+++ app/src/main/java/org/ghostsinthelab/apps/guilelessbopomofo/buffers/PreEditBufferTextView.kt
@@ -61,13 +61,15 @@ class PreEditBufferTextView(context: Context, attrs: AttributeSet) :
                 // 如果使用者點選最後一個字的時候很邊邊角角，
                 // 很可能 getOffsetForPosition() 算出來的值會超界，要扣回來
                 if (offset >= this.text.length) {
-                    offset -= 1
+                    Log.d(logTag, "Offset is over the buffer length!")
+                    offset = this.text.length - 1
                 }
             }
             CursorMovedBy.PHYSICAL_KEYBOARD -> {
                 offset = ChewingBridge.cursorCurrent()
                 if (offset >= ChewingBridge.bufferLen()) {
-                    offset -= 1
+                    Log.d(logTag, "Offset is over the buffer length!")
+                    offset = this.text.length - 1
                 }
             }
         }
