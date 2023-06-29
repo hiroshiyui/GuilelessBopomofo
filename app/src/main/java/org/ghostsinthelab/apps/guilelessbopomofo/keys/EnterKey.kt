55a5ee25962263abc4c5ebf11b2acbff1cca52a7
Hui-Hong You <hiroshi@ghostsinthelab.org>
2023-06-29 15:53:39 +0800

    Revert "[WIP] Enable EventBus again: Add HandleEnterKeyBehaviorEvent"
    This reverts commit 9c526c9d51651e3596920b7785f8514d1292c435.

--- app/src/main/java/org/ghostsinthelab/apps/guilelessbopomofo/keys/EnterKey.kt
+++ app/src/main/java/org/ghostsinthelab/apps/guilelessbopomofo/keys/EnterKey.kt
@@ -20,12 +20,16 @@
 package org.ghostsinthelab.apps.guilelessbopomofo.keys
 
 import android.content.Context
+import android.text.InputType
 import android.util.AttributeSet
+import android.view.KeyEvent
 import android.view.MotionEvent
+import android.view.inputmethod.EditorInfo
 import androidx.core.view.GestureDetectorCompat
 import org.ghostsinthelab.apps.guilelessbopomofo.ChewingBridge
 import org.ghostsinthelab.apps.guilelessbopomofo.ChewingUtil
-import org.ghostsinthelab.apps.guilelessbopomofo.events.HandleEnterKeyBehaviorEvent
+import org.ghostsinthelab.apps.guilelessbopomofo.GuilelessBopomofoServiceContext
+import org.ghostsinthelab.apps.guilelessbopomofo.events.SendSingleDownUpKeyEvent
 import org.ghostsinthelab.apps.guilelessbopomofo.events.UpdateBuffersEvent
 import org.ghostsinthelab.apps.guilelessbopomofo.utils.Vibratable
 import org.greenrobot.eventbus.EventBus
@@ -56,7 +60,37 @@ class EnterKey(context: Context, attrs: AttributeSet) : KeyImageButton(context, 
                 ChewingBridge.handleEnter()
                 EventBus.getDefault().post(UpdateBuffersEvent())
             } else {
-                EventBus.getDefault().post(HandleEnterKeyBehaviorEvent())
+                val editorInfo =
+                    GuilelessBopomofoServiceContext.service.currentInputEditorInfo
+                var multiLineEditText = false
+
+                editorInfo?.let {
+                    // Is it a multiple line text field?
+                    if ((it.inputType and InputType.TYPE_MASK_CLASS and InputType.TYPE_CLASS_TEXT) == InputType.TYPE_CLASS_TEXT) {
+                        if ((it.inputType and InputType.TYPE_MASK_FLAGS and InputType.TYPE_TEXT_FLAG_MULTI_LINE) == InputType.TYPE_TEXT_FLAG_MULTI_LINE) {
+                            multiLineEditText = true
+                        }
+                    }
+
+                    // Just do as press Enter, never care about the defined action if we are now in a multiple line text field
+                    if (multiLineEditText) {
+                        EventBus.getDefault().post(SendSingleDownUpKeyEvent(KeyEvent.KEYCODE_ENTER))
+                        return
+                    }
+
+                    when (val imeAction = (it.imeOptions and EditorInfo.IME_MASK_ACTION)) {
+                        EditorInfo.IME_ACTION_GO, EditorInfo.IME_ACTION_NEXT, EditorInfo.IME_ACTION_SEARCH, EditorInfo.IME_ACTION_SEND -> {
+                            // The current EditText has a specified android:imeOptions attribute.
+                            GuilelessBopomofoServiceContext.service.currentInputConnection.performEditorAction(
+                                imeAction
+                            )
+                        }
+                        else -> {
+                            // The current EditText has no android:imeOptions attribute, or I don't want to make it act as is.
+                            EventBus.getDefault().post(SendSingleDownUpKeyEvent(KeyEvent.KEYCODE_ENTER))
+                        }
+                    }
+                }
             }
         }
     }
