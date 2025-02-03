02418948a8cd6542642f85f2b57d2d95993deb67
Hui-Hong You <hiroshi@ghostsinthelab.org>
2025-02-03 10:31:37 +0800

    Use gradle/libs.versions.toml for libraries version management
    Signed-off-by: Hui-Hong You <hiroshi@ghostsinthelab.org>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,5 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+
 buildscript {
     repositories {
         google()
@@ -6,8 +7,8 @@ buildscript {
     }
 
     dependencies {
-        classpath("com.android.tools.build:gradle:8.8.0")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.24")
+        classpath(libs.gradle)
+        classpath(libs.kotlin.gradle.plugin)
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
