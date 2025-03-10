d8fb49fd7c189e2f9474ec8d5e673a3df4400730
Hui-Hong You <hiroshi@ghostsinthelab.org>
2025-03-10 15:55:37 +0800

    Release 2.1.4
    Signed-off-by: Hui-Hong You <hiroshi@ghostsinthelab.org>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "org.ghostsinthelab.apps.guilelessbopomofo"
         minSdk = 23
         targetSdk = 35
-        versionCode = 128
-        versionName = "2.1.3"
+        versionCode = 129
+        versionName = "2.1.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         externalNativeBuild {
