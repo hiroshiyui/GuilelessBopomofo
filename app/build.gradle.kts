16eb470a407a84347c3222d11697d54aac90f69a
Hui-Hong You <hiroshi@ghostsinthelab.org>
2025-05-20 17:31:40 +0800

    Release 2.2.3-p5
    Signed-off-by: Hui-Hong You <hiroshi@ghostsinthelab.org>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "org.ghostsinthelab.apps.guilelessbopomofo"
         minSdk = 23
         targetSdk = 35
-        versionCode = 137
-        versionName = "2.2.3-p4"
+        versionCode = 138
+        versionName = "2.2.3-p5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         externalNativeBuild {
