d89b521b29596381e28c54b404ad55418c78d950
Hui-Hong You <hiroshi@ghostsinthelab.org>
2025-05-28 19:13:16 +0800

    Use buffer area to switch compact and full IME layout (double tap to toggle it)
    Signed-off-by: Hui-Hong You <hiroshi@ghostsinthelab.org>

---
+++ app/src/main/java/org/ghostsinthelab/apps/guilelessbopomofo/buffers/BufferLayout.kt
@@ -0,0 +1,55 @@
+/*
+ * Guileless Bopomofo
+ * Copyright (C) 2025 YOU, HUI-HONG
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+package org.ghostsinthelab.apps.guilelessbopomofo.buffers
+
+import android.content.Context
+import android.util.AttributeSet
+import android.util.Log
+import android.view.GestureDetector
+import android.view.MotionEvent
+import com.google.android.flexbox.FlexboxLayout
+import org.ghostsinthelab.apps.guilelessbopomofo.events.Events
+import org.greenrobot.eventbus.EventBus
+
+class BufferLayout(context: Context, attrs: AttributeSet) : FlexboxLayout(context, attrs) {
+    var mDetector: GestureDetector
+
+    init {
+        mDetector = GestureDetector(context, MyGestureListener())
+        mDetector.setOnDoubleTapListener(MyGestureListener())
+    }
+
+    inner class MyGestureListener : GestureDetector.SimpleOnGestureListener(), GestureDetector.OnDoubleTapListener {
+        // double tap to toggle compact layout
+        override fun onDoubleTap(e: MotionEvent): Boolean {
+            Log.d("BufferLayout", "onDoubleTap")
+            EventBus.getDefault().post(Events.ToggleForceCompactLayout())
+            return super.onDoubleTap(e)
+        }
+    }
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        // forward event to detector
+        event?.apply {
+            mDetector.onTouchEvent(this)
+        }
+        return true
+    }
+}
