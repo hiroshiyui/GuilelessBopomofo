5f9876c5efcb356f88fdfb919c4afbffa94cf19e
Hui-Hong You <hiroshi@ghostsinthelab.org>
2025-09-07 00:02:49 +0800

    Deprecated typealias RegisteredSharedPreferences
    Signed-off-by: Hui-Hong You <hiroshi@ghostsinthelab.org>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -79,7 +79,6 @@ android {
     kotlin {
         compilerOptions {
             jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
-            freeCompilerArgs.add("-Xnested-type-aliases")
         }
     }
 
